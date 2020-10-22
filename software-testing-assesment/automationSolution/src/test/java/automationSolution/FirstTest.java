d611eb8685148d7de4bd4579eba217715c793b57
Dmytro Popazov <saiferdima@gmail.com>
2020-10-22 17:43:53 +0300

    fix issues Finished firstTest

--- software-testing-assesment/automationSolution/src/test/java/automationSolution/FirstTest.java
+++ software-testing-assesment/automationSolution/src/test/java/automationSolution/FirstTest.java
@@ -1,15 +1,15 @@
 package automationSolution;
 import org.junit.Test;
-import static org.junit.Assert.*;
+import static com.codeborne.selenide.Condition.text;
 import org.openqa.selenium.By;
-
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
+import static com.codeborne.selenide.CollectionCondition.texts;
 
 public class FirstTest {
     @Test
-    public void firstTry(){
+    public void userShouldBeAbleToCreateOrder(){
         open("https://www.saucedemo.com/index.html");
         $(By.id("user-name")).setValue("standard_user");
         $(By.id("password")).setValue("secret_sauce").pressEnter();
@@ -24,12 +24,14 @@ public class FirstTest {
         $$(".cart_item").shouldHaveSize(2);
         String firstValueInBasket = $$(".cart_item .inventory_item_name").get(0).getText();
         String secondValueInBasket = $$(".cart_item .inventory_item_name").get(1).getText();
-        assertEquals(firstItemName,firstValueInBasket);
-        assertEquals(secondItemName,secondValueInBasket);
-        //click checkut
-        //fill data
-        //assert price
-        //click finish
-        //verify thank you page
+        $$(".cart_item .inventory_item_name").shouldHave(texts(firstItemName,secondItemName));
+        $("a.btn_action.checkout_button").click();
+        $("#first-name").setValue("FName");
+        $("#last-name").setValue("LName");
+        $("#postal-code").setValue("61060");
+        $("input.btn_primary.cart_button").click();
+        Integer totalitemPrice = Integer.parseInt($(".summary_subtotal_label").getText().replaceAll("\\D", ""));
+        $("a.btn_action.cart_button").click();
+        $("#checkout_complete_container > h2").shouldHave(text("THANK YOU FOR YOUR ORDER!"));
     }
 }
